93ccf863f679c939cd676642f82176ee039b1471
kenji.takada <falcon9628@yahoo.co.jp>
2021-01-21 20:28:36 +0000

    CRUD messages

---
+++ app/Http/Controllers/MessagesController.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Message;    // 追加
+
+
+class MessagesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+     
+    // getでmessages/にAccessされた場合の「一覧表示処理」
+    public function index()
+    {
+        // Messageを全部取得
+        $messages = Message::all();
+
+        return view('messages.index', [
+            'messages' => $messages,
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    // getでmessages/createにAccessされた場合の「新規登録画面表示処理」
+    public function create()
+    {
+        // Messageを作成
+        $message = new Message;
+        
+        return view('messages.create', [
+            'message' => $message,
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    // postでmessages/にAccessされた場合の「新規登録処理」
+    public function store(Request $request)
+    {
+        
+        $message = new Message;
+        $message->content = $request->content;
+        $message->save();
+        // request化　更に　内容　保存
+
+        // redirectする
+        return redirect('/');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    // getでmessages/idにAccessされた場合の「取得表示処理」
+    public function show($id)
+    {
+        $message = Message::find($id);
+        return view('messages.show', [
+            'message' => $message,
+        ]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    // getでmessages/id/editにAccessされた場合の「更新画面表示処理」
+    public function edit($id)
+    {
+        $message = Message::find($id);
+        // $messageにはcontent が最初から入っている
+
+        return view('messages.edit', [
+            'message' => $message,
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    // putまたはpatchでmessages/idにAccessされた場合の「更新処理」
+    public function update(Request $request, $id)
+    {
+        $message = Message::find($id);
+        $message->content = $request->content;
+        $message->save();
+
+        // redirectする
+        return redirect('/');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    // deleteでmessages/idにAccessされた場合の「削除処理」
+    public function destroy($id)
+    {
+        $message = Message::find($id);
+        $message->delete();
+
+        return redirect('/');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,41 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// Route::get('/', function () {
+//     return view('index');
+// });
+
+Route::get('/','MessagesController@index');
+
+// // CRUD  省略記述版
+// これだけでCRUDから　全部　指定　show　store　update　destroy
+Route::resource('messages', 'MessagesController');
+
+
+
+// // CRUD  詳細記述版
+
+// // Message個別詳細Page表示
+// Route::get('messages/{id}', 'MessagesController@show');
+
+// // Message新規登録処理
+// Route::post('messages', 'MessagesController@store');
+
+// // Message の更新処理
+// Route::put('messages/{id}', 'MessagesController@update');
+
+// // Message の削除
+// Route::delete('messages/{id}', 'MessagesController@destroy');
+
+// // index:showの補助Page Resauseの一覧Pageへ
+// Route::get('messages', 'MessagesController@index')->name('messages.index');
+
+// // store 新規作成(create)用 FormPage作成
+// Route::get('messages/create', 'MessagesController@create')->name('messages.create');
+
+// // edit 更新(updata)用　FormPage作成
+// Route::put('messages/{id}', 'MessagesController@update')->name('messages.edit');
